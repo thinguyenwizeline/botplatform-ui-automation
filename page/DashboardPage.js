45d3dd84297af9e2c7c80b8460800d22fc88b3af
thinguyenwizeline <thi.nguyen@wizeline.com>
2017-10-25 16:34:17 +0700

    ## Delete report existing and commit behaviour click button Create Bot ##

--- page/DashboardPage.js
+++ page/DashboardPage.js
@@ -1,5 +1,8 @@
 this.DashboardPage = $page("Dashboard page",{  
-    createBot : "xpath : //button[@class='action-btn sc-bxivhb iseeCv']",
+    createBot : "xpath : //button[@class='action-btn sc-bxivhb jTIGHO']",
 },{
-
+    clickCreateBot: loggedFunction("Click to Create Bot", function(user){
+        this.createBot.click();
+        console.log("Click Create Bot");       
+    })
 });
